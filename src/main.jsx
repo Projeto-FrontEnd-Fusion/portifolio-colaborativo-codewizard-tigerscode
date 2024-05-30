b2310b38111a7b0ba018ecff3a9427ec4e0ec98f
Davi Santanna <davicalvalcanti@gmail.com>
2024-05-29 22:01:29 -0300

    STYLE: "added style to the main"

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <main>
-    <span>[Coloque aqui o hero]</span>
+    <main className="flex flex-col">
+      <span>[Coloque aqui o hero]</span>
       <span>[Coloque aqui a sessão de benefits]</span>
-      <span>[Coloque aqui a sesão de tech=amd-lang]</span>
+      <span>[Coloque aqui a sesão de tech-and-lang]</span>
       <span>[Coloque aqui a sesão de team]</span>
       <span>[Coloque aqui a sesão de aboutProject]</span>
       <span>[Coloque aqui a sesão de coments]</span>
