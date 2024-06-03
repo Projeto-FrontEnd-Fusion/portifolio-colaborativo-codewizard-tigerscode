6c17734889c9ff6c11712a88cfd0aafe1f7b469f
Matheus Amon <amonmatheus757@gmail.com>
2024-06-03 09:46:44 -0300

    chore: add custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,40 @@
 /** @type {import('tailwindcss').Config} */
+
+import plugin from 'tailwindcss/plugin'
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'button-primary-bg': '#16C586',
+        'button-secondary-bg': '#0368FF'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities }) {
+      addUtilities({
+        '.github-button': {
+          'width': '128px',
+          'height': '48px',
+          'border-radius': '8px',
+          'display': 'flex',
+          'align-items': 'center',
+          'justify-content': 'center',
+          'gap': '8px',
+          'cursor': 'pointer',
+        },
+        '.title-font': {
+          'font-family': 'ZenDots, sans-serif',
+        },
+        '.subtitle-font': {
+          'font-family': 'Inconsolata, sans-serif',
+        },
+        '.body-font': {
+          'font-family': 'Mulish, sans-serif',
+        }
+      })
+    })
+  ],
 };
