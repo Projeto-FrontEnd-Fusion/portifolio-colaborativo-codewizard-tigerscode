1504ba041a006397b772ca6b14fda76f74e101be
Matheus Amon <amonmatheus757@gmail.com>
2024-06-04 16:16:34 -0300

    feat: add link to button

--- src/components/hero/components/git-button.jsx
+++ src/components/hero/components/git-button.jsx
@@ -3,11 +3,18 @@ import { LuGithub } from "react-icons/lu";
 
 const GitButton = ({ primary }) => {
   return (
-    <button
-      className={`github-button hover:bg-btn-secondary hover:text-white ${primary ? "bg-btn-primary" : "bg-btn-secondary text-white hover:bg-btn-primary"}`}
+    <button 
+      className={`github-button hover:text-white ${primary ? "bg-btn-primary hover:bg-btn-secondary" : "bg-btn-secondary text-white hover:bg-btn-primary"}`}
     >
-      <span>github</span>
-      <LuGithub size={16} strokeWidth={3} />
+      <a 
+        href="https://github.com/Projeto-FrontEnd-Fusion/portifolio-colaborativo-codewizard-tigerscode" 
+        target="_blank" 
+        rel="nofollow" 
+        className="flex items-center gap-[10px]"
+      >
+        <span>github</span>
+        <LuGithub size={16} strokeWidth={3} />
+      </a>
     </button>
   );
 };
