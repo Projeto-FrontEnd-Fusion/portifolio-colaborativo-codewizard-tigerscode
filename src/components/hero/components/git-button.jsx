1675181e458714ccdea81acce608abdcef582ded
Matheus Amon <amonmatheus757@gmail.com>
2024-06-04 10:10:26 -0300

    feat: add prop types to github button

--- src/components/hero/components/git-button.jsx
+++ src/components/hero/components/git-button.jsx
@@ -1,9 +1,10 @@
+import PropTypes from "prop-types"
 import { LuGithub } from "react-icons/lu"
 
-const GitButton = () => {
+const GitButton = ({ primary }) => {
     return ( 
         <button 
-            className="github-button hover:bg-btn-secondary hover:text-white bg-btn-primary"
+            className={`github-button hover:bg-btn-secondary hover:text-white ${primary ? 'bg-btn-primary' : 'bg-btn-secondary text-white hover:bg-btn-primary'}`}
         >
             <span>
                 github
@@ -11,6 +12,14 @@ const GitButton = () => {
             <LuGithub size={16} strokeWidth={3} />
         </button>
      );
+}
+
+GitButton.propTypes = {
+    primary: PropTypes.bool
+}
+
+GitButton.defaultProps = {
+    primary: false
 }
  
 export default GitButton;
