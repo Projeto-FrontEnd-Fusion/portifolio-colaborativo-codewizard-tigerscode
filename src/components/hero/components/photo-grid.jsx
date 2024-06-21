18f8c0ff6873643539f20a8db423cf3e1a6e16b1
Matheus Amon <amonmatheus757@gmail.com>
2024-06-21 09:56:09 -0300

    refactor: add a parent div to photo grid

--- src/components/hero/components/photo-grid.jsx
+++ src/components/hero/components/photo-grid.jsx
@@ -6,24 +6,26 @@ const PhotoGrid = () => {
   //const { teamData } = useContext(TeamContext);
 
   return (
-    <section className="relative grid w-full grid-flow-col grid-cols-4 items-center gap-2 py-0 md:gap-3 lg:w-auto lg:gap-4 lg:py-20">
-      <div className="relative row-span-2 aspect-[80/95] min-w-14 max-w-40">
-        <img className="h-full w-full rounded-2xl bg-[#C8C8C8] object-cover lg:rounded-[32px] dark:bg-[#221C3E]" />
-      </div>
-      <div className="relative aspect-[80/95] min-w-14 max-w-40 lg:bottom-20">
-        <img className="h-full w-full rounded-2xl bg-[#C8C8C8] object-cover lg:rounded-[32px] dark:bg-[#221C3E]" />
-      </div>
-      <div className="relative aspect-[80/95] min-w-14 max-w-40 lg:bottom-20">
-        <img className="h-full w-full rounded-2xl bg-[#C8C8C8] object-cover lg:rounded-[32px] dark:bg-[#221C3E]" />
-      </div>
-      <div className="relative top-4 aspect-[80/95] min-w-14 max-w-40 lg:top-10">
-        <img className="h-full w-full rounded-2xl bg-[#C8C8C8] object-cover lg:rounded-[32px] dark:bg-[#221C3E]" />
-      </div>
-      <div className="relative top-4 aspect-[80/95] min-w-14 max-w-40 lg:top-10">
-        <img className="h-full w-full rounded-2xl bg-[#C8C8C8] object-cover lg:rounded-[32px] dark:bg-[#221C3E]" />
-      </div>
-      <div className="relative top-4 row-span-2 aspect-[80/95] min-w-14 max-w-40 lg:top-0 lg:block">
-        <img className="h-full w-full rounded-2xl bg-[#C8C8C8] object-cover lg:w-40 lg:rounded-[32px] dark:bg-[#221C3E]" />
+    <section className="w-full md:px-20 lg:w-auto lg:px-0 lg:py-20">
+      <div className="grid grid-flow-col grid-cols-4 items-center gap-2 py-0 md:gap-3 lg:gap-4">
+        <div className="relative row-span-2 aspect-[80/95] min-w-14 max-w-40">
+          <img className="h-full w-full rounded-2xl bg-[#C8C8C8] object-cover lg:rounded-[32px] dark:bg-[#221C3E]" />
+        </div>
+        <div className="relative aspect-[80/95] min-w-14 max-w-40 lg:bottom-20">
+          <img className="h-full w-full rounded-2xl bg-[#C8C8C8] object-cover lg:rounded-[32px] dark:bg-[#221C3E]" />
+        </div>
+        <div className="relative aspect-[80/95] min-w-14 max-w-40 lg:bottom-20">
+          <img className="h-full w-full rounded-2xl bg-[#C8C8C8] object-cover lg:rounded-[32px] dark:bg-[#221C3E]" />
+        </div>
+        <div className="relative top-4 aspect-[80/95] min-w-14 max-w-40 lg:top-10">
+          <img className="h-full w-full rounded-2xl bg-[#C8C8C8] object-cover lg:rounded-[32px] dark:bg-[#221C3E]" />
+        </div>
+        <div className="relative top-4 aspect-[80/95] min-w-14 max-w-40 lg:top-10">
+          <img className="h-full w-full rounded-2xl bg-[#C8C8C8] object-cover lg:rounded-[32px] dark:bg-[#221C3E]" />
+        </div>
+        <div className="relative top-4 row-span-2 aspect-[80/95] min-w-14 max-w-40 lg:top-0 lg:block">
+          <img className="h-full w-full rounded-2xl bg-[#C8C8C8] object-cover lg:w-40 lg:rounded-[32px] dark:bg-[#221C3E]" />
+        </div>
       </div>
     </section>
   );
--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
       <Header />
 
       <div className="mb-[114px] flex flex-col items-center justify-between space-y-10 px-10 md:px-[93px] lg:mt-[66px] lg:flex-row-reverse">
-        <div>
+        <div className="w-full lg:w-fit">
           <section className="pt-[35px]">
             <h1 className="space-y-2 text-wrap text-center lg:text-right">
               <span className="block font-subtitle text-[18px] text-primary-muted lg:text-[28px] dark:text-gray-text">
