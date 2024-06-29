461f6ef3882e0b0d460f3f3a4e81704ae23bde23
Davi Santanna <davicalvalcanti@gmail.com>
2024-06-29 17:28:07 -0300

    REFACTOR: refatoring in media queries and dark / ligth mode

--- src/components/benefits/index.jsx
+++ src/components/benefits/index.jsx
@@ -1,28 +1,33 @@
-import ClickIcon from '../../assets/iconclick.svg';
-import CollabIcon from '../../assets/iconcollab.svg';
-import SafeIcon from '../../assets/iconsafe.svg';
-import TaskIcon from '../../assets/icontask.svg';
-import ViewIcon from '../../assets/iconview.svg';
+import ClickIcon from "../../assets/iconclick.svg";
+import CollabIcon from "../../assets/iconcollab.svg";
+import SafeIcon from "../../assets/iconsafe.svg";
+import TaskIcon from "../../assets/icontask.svg";
+import ViewIcon from "../../assets/iconview.svg";
+import PropTypes from "prop-types";
 
 const Card = ({ title, subtitle, icon, hasShadow, isSpecial }) => {
   return (
-    <div className={`w-[600px] h-[290px] p-8 rounded-lg opacity-100 mb-4 mx-2 ${hasShadow ? 'shadow-lg bg-white dark:bg-pirmary-muted' : 'shadow-none bg-transparent'}`}>
+    <div
+      className={`mx-2 mb-4 h-auto rounded-lg p-8 opacity-100 ${hasShadow ? "dark:bg-pirmary-muted dark:bg-primary-muted bg-white shadow-lg" : "bg-transparent shadow-none"} max-w-[400px]:w-[200px] max-sm:w-[80%] sm:w-[400px] md:w-[400px] lg:w-[480px] `}
+    >
       {isSpecial && (
         <>
-          <div className="font-subtitle font-extrabold text-4xl text-primary dark:text-white mb-5">
+          <div className="mb-5 font-subtitle text-4xl font-extrabold text-primary dark:text-white">
             {title}
           </div>
-          <hr className="border-t-2 border-primary dark:border-white mb-4" />
-          <p className="font-semibold text-lg text-primary dark:text-gray-text dark:font-normal leading-snug">
+          <hr className="mb-4 border-t-2 border-primary dark:border-white" />
+          <p className="text-lg font-semibold leading-snug text-primary dark:font-normal dark:text-gray-text">
             {subtitle}
           </p>
         </>
       )}
       {!isSpecial && (
         <>
-          <div className="flex items-center mb-4">
-            <img src={icon} alt="" className="w-6 h-6 mr-2" />
-            <div className="text-xl text-primary dark:text-white font-bold">{title}</div>
+          <div className="mb-4 flex items-center">
+            <img src={icon} alt="" className="mr-2 h-6 w-6" />
+            <div className="text-xl font-bold text-primary dark:text-white">
+              {title}
+            </div>
           </div>
           <p className="text-primary dark:text-white">{subtitle}</p>
         </>
@@ -31,45 +36,53 @@ const Card = ({ title, subtitle, icon, hasShadow, isSpecial }) => {
   );
 };
 
+Card.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  icon: PropTypes.Image,
+  hasShadow: PropTypes.Boolean,
+  isSpecial: PropTypes.Boolean,
+};
+
 const Beneficios = () => {
   return (
     <div className="flex items-center justify-center font-body dark:font-body">
-      <div className="mt-8 mb-20 flex flex-wrap justify-center gap-4">
+      <div className="mb-20 mt-8 flex flex-wrap justify-center gap-4">
         <Card
           title="Vantagens do Projeto"
           subtitle="Participar de um projeto em grupo para aprender programação web frontend e desenvolver um portfólio colaborativo oferece uma série de vantagens!"
-          hasShadow={false} 
+          hasShadow={false}
           isSpecial={true}
         />
         <Card
           title="Diversidade de habilidades:"
           subtitle="Ao trabalhar em equipe, você pode aprender com os outros membros do grupo. Cada pessoa traz suas habilidades e conhecimentos únicos, permitindo trocas constantes de aprendizado e a oportunidade de explorar diferentes abordagens para resolver problemas."
           icon={ClickIcon}
-          hasShadow={true} 
+          hasShadow={true}
         />
         <Card
           title="Divisão de tarefas:"
           subtitle="Trabalhar em equipe permite distribuir o trabalho de forma eficiente, acelerando o progresso e permitindo que cada membro se concentre em suas áreas de especialização."
           icon={TaskIcon}
-          hasShadow={true} 
+          hasShadow={true}
         />
         <Card
           title="Aprendizado colaborativo:"
           subtitle="Ao trabalhar em equipe, você pode aprender com os outros membros do grupo. Cada pessoa traz suas habilidades e conhecimentos únicos, permitindo trocas constantes de aprendizado e a oportunidade de explorar diferentes abordagens para resolver problemas."
           icon={CollabIcon}
-          hasShadow={true} 
+          hasShadow={true}
         />
         <Card
           title="Diversidade de perspectivas:"
           subtitle="Trabalhar com pessoas diferentes traz uma variedade de perspectivas e abordagens para resolver problemas. Isso pode levar a soluções mais criativas e inovadoras, já que cada membro do grupo pode contribuir com suas ideias únicas."
           icon={ViewIcon}
-          hasShadow={true} 
+          hasShadow={true}
         />
         <Card
           title="Habilidades interpessoais:"
           subtitle="Trabalhar em equipe ajuda a aprimorar habilidades de comunicação, colaboração e trabalho em grupo. Essas habilidades são essenciais no ambiente de trabalho, não apenas na área de programação, mas em muitos outros campos também."
           icon={SafeIcon}
-          hasShadow={true} 
+          hasShadow={true}
         />
       </div>
     </div>
