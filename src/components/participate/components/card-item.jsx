c680c8122e0575344f65e26cf6eb23d3fe571ca3
Matheus Amon <amonmatheus757@gmail.com>
2024-09-18 17:41:43 -0300

    feat: ensure Participant component is responsive

--- src/components/participate/components/card-item.jsx
+++ src/components/participate/components/card-item.jsx
@@ -1,19 +1,25 @@
 import PropTypes from "prop-types";
 
-const CardItem = ({emoji, title, text}) => {
-    return ( 
-        <div className="p-8 bg-[#7676764D] rounded-lg">
-            <p className="text-5xl mb-6">{emoji}</p>
-            <h3 className="font-body text-sm font-bold text-primary">{title}:</h3>
-            <p className="font-body text-sm text-primary">{text}</p>
-        </div>
-     );
-}
- 
+const CardItem = ({ emoji, title, text }) => {
+  return (
+    <div className="flex flex-col rounded-lg bg-[#7676764D] p-8 sm:min-w-[364px] lg:flex-row lg:gap-8">
+      <p className="mb-6 text-5xl lg:text-[64px]">{emoji}</p>
+      <div>
+        <h3 className="font-body text-sm font-bold leading-normal text-primary lg:text-[18px]">
+          {title}:
+        </h3>
+        <p className="font-body text-sm leading-normal text-primary lg:text-[18px]">
+          {text}
+        </p>
+      </div>
+    </div>
+  );
+};
+
 export default CardItem;
 
 CardItem.propTypes = {
-    emoji: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-}
+  emoji: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
--- src/components/participate/index.jsx
+++ src/components/participate/index.jsx
@@ -1,18 +1,42 @@
 import CardItem from "./components/card-item";
 
 const Participate = () => {
-    return ( 
-        <div className="px-8">
-            <div className="space-y-4">
-                <h2 className="font-body text-center font-black text-[28px]">Sobre o Projeto Frontend Fusion</h2>
-                <p className="font-body text-center text-sm">
-                    O Frontend Fusion é um projeto inovador focado em proporcionar uma experiência envolvente de aprendizado de programação. Nosso objetivo é oferecer um ambiente colaborativo onde entusiastas de tecnologia possam mergulhar no mundo do desenvolvimento frontend, explorando CSS, HTML e JavaScript através de desafios e atividades interativas.
-                </p>
-            </div>
+  return (
+    <div className="mb-[270px] px-8 lg:px-[101px]">
+      <div className="mb-20 space-y-4">
+        <h2 className="text-center font-body text-[28px] font-black lg:text-[48px]">
+          Sobre o Projeto Frontend Fusion
+        </h2>
+        <p className="text-center font-body text-sm leading-normal lg:text-[22px]">
+          O Frontend Fusion é um projeto inovador focado em proporcionar uma
+          experiência envolvente de aprendizado de programação. Nosso objetivo é
+          oferecer um ambiente colaborativo onde entusiastas de tecnologia
+          possam mergulhar no mundo do desenvolvimento frontend, explorando CSS,
+          HTML e JavaScript através de desafios e atividades interativas.
+        </p>
+      </div>
 
-            <CardItem emoji="💼" title="O que procuramos" text="Paixão pela programação e desejo de aprender e crescer. Conhecimento básico de CSS, HTML e JavaScript (ou disposição para aprender).Capacidade de trabalhar em equipe e colaborar em projetos conjuntos. Comprometimento e vontade de enfrentar desafios e superar obstáculos." />
-        </div>
-     );
-}
- 
+      <div className="flex flex-col items-center space-y-8">
+        <CardItem
+          emoji="💼"
+          title="O que procuramos"
+          text="Paixão pela programação e desejo de aprender e crescer. Conhecimento básico de CSS, HTML e JavaScript (ou disposição para aprender).Capacidade de trabalhar em equipe e colaborar em projetos conjuntos. Comprometimento e vontade de enfrentar desafios e superar obstáculos."
+        />
+
+        <CardItem
+          emoji="🎯"
+          title="Responsabilidades"
+          text="Participar de desafios e atividades gamificadas para desenvolver habilidades em programação.Colaborar com outros membros da equipe para criar projetos frontend inovadores.Contribuir com ideias e feedback para aprimorar continuamente o projeto."
+        />
+
+        <CardItem
+          emoji="🌟"
+          title="Benefícios"
+          text="Acesso a uma comunidade de aprendizado dinâmica e colaborativa. Oportunidade de desenvolver habilidades práticas em programação. Experiência valiosa para enriquecer seu currículo e portfólio."
+        />
+      </div>
+    </div>
+  );
+};
+
 export default Participate;
