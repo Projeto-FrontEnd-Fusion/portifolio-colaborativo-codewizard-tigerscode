4d25f02481182594ac0a97a217c7f4b67efaed92
Matheus Amon <amonmatheus757@gmail.com>
2024-09-19 19:39:50 -0300

    feat: add participate call to action

--- src/components/participate/index.jsx
+++ src/components/participate/index.jsx
@@ -2,7 +2,7 @@ import CardItem from "./components/card-item";
 
 const Participate = () => {
   return (
-    <div className="mb-[270px] px-8 lg:px-[101px]">
+    <div className="mb-[270px] px-8 lg:px-[100px]">
       <div className="mb-20 space-y-4">
         <h2 className="text-center font-body text-[28px] font-black lg:text-[48px]">
           Sobre o Projeto Frontend Fusion
@@ -34,6 +34,18 @@ const Participate = () => {
           title="Benefícios"
           text="Acesso a uma comunidade de aprendizado dinâmica e colaborativa. Oportunidade de desenvolver habilidades práticas em programação. Experiência valiosa para enriquecer seu currículo e portfólio."
         />
+      </div>
+
+      <div className="mx-auto mt-[180px] flex w-full max-w-[750px] flex-col items-center gap-4 lg:gap-10">
+        <p className="text-center font-body text-primary lg:text-[32px]">
+          Estamos ansiosos para receber sua inscrição e dar as boas-vindas a
+          você em nossa comunidade!
+        </p>
+        <a href="#">
+          <button className="min-h-[42px] w-[216px] max-w-[459px] rounded-lg bg-btn-primary px-8 py-4 font-body text-lg font-bold text-white-tree transition-colors duration-500 hover:bg-btn-secondary lg:w-[459px] lg:rounded-2xl lg:px-12 lg:py-4 lg:text-4xl">
+            Quero Participar
+          </button>
+        </a>
       </div>
     </div>
   );
